e210fa58faf3be1b20e6aca947dbc690cf569707
barcek2281 <sabdp17@gmail.com>
2024-10-27 16:47:07 +0500

    profile

--- js/navBar.js
+++ js/navBar.js
@@ -35,4 +35,49 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
   });
+
+
+
+// ---------------------------LOGIN PAGE--------------------------------------
+
+
+const usernameDisplay = document.getElementById('username-display');
+const loginLink = document.getElementById('login-link');
+const logoutLink = document.getElementById('logout-link');
+
+function checkUserSession() {
+    const username = localStorage.getItem('username');
+    
+    if (username) {
+        usernameDisplay.textContent = `${username}`;
+        usernameDisplay.style.display = 'inline';
+        loginLink.style.display = 'none';
+        logoutLink.style.display = 'inline';
+    } else {
+        usernameDisplay.style.display = 'none';
+        loginLink.style.display = 'inline';
+        logoutLink.style.display = 'none';
+    }
+}
+
+loginLink.addEventListener('click', function(event) {
+    event.preventDefault();  
+    
+    const username = prompt("Enter your username:");
+    if (username) {
+        localStorage.setItem('username', username);
+        checkUserSession();
+        alert(`Welcome, ${username}!`);
+    }
+});
+
+logoutLink.addEventListener('click', function(event) {
+    event.preventDefault();  
+    
+    localStorage.removeItem('username');
+    checkUserSession();
+});
+
+window.onload = checkUserSession;
+
   
